c02c0a21876cf3c30859928645cba11b4eefd780
fvonsovs <filipvon@protonmail.com>
2023-02-21 18:04:09 +0100

    Add header and pipex.c files

---
+++ pipex.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 15:09:18 by fvonsovs          #+#    #+#             */
+/*   Updated: 2023/02/21 18:04:06 by fvonsovs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+// Parse the command-line arguments. 
+// Open file1 for reading and file2 for writing.
+// Use the pipe function to create a pipe between the two commands. 
+// This allows the output of cmd1 to be fed as input to cmd2.
+// Fork the process. This creates a child process that will execute cmd1.
+// In the child process, use dup2 to redirect the standard output 
+// to the write end of the pipe. 
+// This allows the output of cmd1 to be written to the pipe.
+// In the parent process, fork again to 
+// create a second child process that will execute cmd2.
+// In the second child process, use dup2 to redirect 
+// the standard input to the read end of the pipe.
+// This allows the input to cmd2 to come from the pipe.
+// Use execve to execute cmd1 in the first 
+// child process and cmd2 in the second child process.
+// Use wait or waitpid to wait for both 
+// child processes to finish executing.
+// Close any open file descriptors and 
+// free any allocated memory to avoid memory leaks.
+// Handle any errors that may occur, using the 
+// external functions listed in the project requirements as necessary.
+
+
+// The char *envp[] argument is a pointer to an 
+// array of strings that represent the environment
+// variables for the current process. In C, environment variables
+// are stored as a list of key-value pairs, where the keys and values are strings.
+// The char *envp[] argument is a way to access the environment variables 
+// in a more flexible way. It is typically used in programs
+// that need to modify the environment variables or pass
+// a modified set of environment variables to a child process.
+int	main(int argc, char *argv[], char *envp[])
+{
+ 	if (argc != 5)
+ 		return (ft_printf("Usage: %s <file1> <cmd1> <cmd2> <file2>\n", argv[0]););
+
+
+
+
+
+
+	
+}
---
+++ pipex.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 15:17:27 by fvonsovs          #+#    #+#             */
+/*   Updated: 2023/02/21 15:30:36 by fvonsovs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <string.h>
+# include <stdio.h>
+
+# include "./libft/libft.h"
+
+
+
+#endif
