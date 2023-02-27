c3aa57e421d6e4eb758bf2766fecc01b99b13adc
fvonsovs <filipvon@protonmail.com>
2023-02-27 15:49:49 +0100

    Added here_doc functions

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 15:09:18 by fvonsovs          #+#    #+#             */
-/*   Updated: 2023/02/27 15:07:26 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2023/02/27 15:49:42 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,11 +77,71 @@ int	you_fucked_up(char msg)
 	exit(0);
 }
 
-int	here_doc(int in, int out, char cmd1, char cmd2)
+void	here_doc(char **argv)
+{
+	int		pid_fd[2];
+	pid_t	pid;
+
+	if (pipe(pid_fd) == -1)
+		exit(0);
+	pid = fork();
+	if (pid == -1)
+		exit(0);
+	if (!pid)
+		here_doc_exec(argv, pid_fd);
+	else
+	{
+		close(pid_fd[1]);
+		dup2(pid_fd[0], 0);
+		wait(NULL);
+	}
+}
+
+void	here_doc_exec(char **argv, int *pid_fd)
+{
+	char	*line;
+
+	close(pid_fd[0]);
+	while (1)
+	{
+		line = get_next_line(0);
+		if (ft_strncmp(line, argv[2], ft_strlen(argv[2])) == 0)
+		{
+			free(ret);
+			exit(0);
+		}
+		ft_printf("%s", line);
+		free(line);
+	}
+}
+
+void	pipex(char *cmd, char **envp)
+{
+	pid_t	pid;
+	int		pid_fd[2];
+
+	if (pipe(pid_fd) == -1)
+		exit(0);
+	pid = fork();
+	if (pid == -1)
+		exit(0);
+	if (!pid)
+	{
+		close(pid_fd[0]);
+		dup2(pid_fd[1], 1);
+		execute(cmd, envp);
+	}
+	else
+	{
+		close(pid_fd[1]);
+		dup2(pid_fd[0], 0);
+	}
+}
+
+void	execute(char *cmd, char **envp)
 {
 
 
 
-
-
+	
 }
